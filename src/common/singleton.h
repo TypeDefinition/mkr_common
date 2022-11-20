e93179cc8c5ac9397733c6dd0859df817d1cfa8d
TypeDefinition <lnxterry@gmail.com>
2022-11-20 17:25:30 +0800

    add comparators, concepts, family & singleton

---
+++ src/common/singleton.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+
+namespace mkr {
+    template<typename T>
+    class singleton {
+    private:
+        static T* instance_;
+
+    protected:
+        singleton() {}
+
+        virtual ~singleton() {}
+
+    public:
+        static T& instance() {
+            return instance_ ? *instance_ : *(instance_ = new T());
+        }
+
+        static void destroy() {
+            if (!instance_) { return; }
+            delete instance_;
+            instance_ = nullptr;
+        }
+    };
+
+    template<typename T>
+    T* singleton<T>::instance_ = nullptr;
+}
