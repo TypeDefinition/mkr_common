79c366cf0eb1e7e91095c84149bc00ff58fb20c7
TypeDefinition <lnxterry@gmail.com>
2024-02-22 18:22:43 +0800

    Update type_id

--- src/common/type_id.h
+++ src/common/type_id.h
@@ -8,11 +8,11 @@ namespace mkr {
     typedef uint64_t type_id_t;
 
     /**
-     * Utility class to generate a unique id for a group of classes.
-     * Each child class of Base will have a type id generated starting from 0.
-     * @tparam Base The base class of the category.
+     * Utility class to generate a unique id for classes. Different classes are guaranteed to get different ids in each application run, but may be assigned different ids between runs.
+     * @tparam Category Used to allow multiple instances of type_id to exists in a single application.
+     * Example: type_id<foo> for foo-related classes, and type_id<bar> for bar-related classes. Then, type_id<foo>::value<baz>() might not equal type_id<bar>::value<baz>().
      */
-    template<typename Base>
+    template <typename Category>
     class type_id {
     private:
         /// Current type id.
@@ -20,10 +20,11 @@ namespace mkr {
 
         /**
          * Generate the type id of a class. This exists so that get_id() can remove the cv of the template class.
-         * @tparam Derived The derived class of Base. Derived may also be of type Base. This type is cv removed.
-         * @return The type id of class Derived.
+         * Subsequent calls to generate of the same type will return the same value.
+         * @tparam T The type to generate the id of. This type is cv removed.
+         * @return The type id of class T.
          */
-        template<typename Derived>
+        template<typename T>
         static type_id_t generate() {
             static type_id_t id = current_id_++;
             return id;
@@ -34,16 +35,16 @@ namespace mkr {
 
         /**
          * Get the type id of a class.
-         * @tparam Derived The class type. Derived may also be of type Base.
-         * @return The type id of Derived.
+         * @tparam The class type.
+         * @return The type id of T.
          */
-        template<typename Derived>
-        static type_id_t value() requires std::is_base_of_v<Base, Derived> {
-            return generate<std::remove_cvref_t<Derived>>();
+        template<typename T>
+        static type_id_t value() {
+            return generate<std::remove_cvref_t<T>>();
         }
     };
 
     /// Type id starts from 0.
-    template<typename Base>
-    std::atomic<type_id_t> type_id<Base>::current_id_ = 0;
+    template <typename Category>
+    std::atomic<type_id_t> type_id<Category>::current_id_ = 0;
 }
--- test/ut/type_id_test.cpp
+++ test/ut/type_id_test.cpp
@@ -3,16 +3,28 @@
 
 using namespace mkr;
 
-TEST(type_id, base_derived) {
-    struct base {};
-    struct derived_a : public base {};
-    struct derived_b : public base {};
-    struct foo {};
+TEST(type_id, category) {
+    struct category_a {};
+    struct category_b {};
 
-    auto id_1 = type_id<base>::value<derived_a>();
-    auto id_2 = type_id<base>::value<derived_a>();
-    auto id_3 = type_id<base>::value<derived_b>();
+    struct foo {};
+    struct bar {};
+    struct baz {};
+
+    auto id_1 = type_id<category_a>::value<foo>();
+    auto id_2 = type_id<category_a>::value<foo>();
+    auto id_3 = type_id<category_a>::value<baz>();
 
     EXPECT_TRUE(id_1 == id_2);
-    EXPECT_TRUE(id_1 != id_3);
+    EXPECT_FALSE(id_1 == id_3);
+
+    auto id_4 = type_id<category_b>::value<foo>();
+    auto id_5 = type_id<category_b>::value<bar>();
+    auto id_6 = type_id<category_b>::value<baz>();
+
+    EXPECT_FALSE(id_4 == id_5);
+    EXPECT_FALSE(id_5 == id_6);
+    EXPECT_FALSE(id_4 == id_6);
+    EXPECT_TRUE(id_1 == id_4);
+    EXPECT_FALSE(id_3 == id_6);
 }
